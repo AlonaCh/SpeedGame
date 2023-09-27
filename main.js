e3b2b56f9c094426e6545ad20de4c435fd2950c4
Alona Chubenko <aliona.chubenko@gmail.com>
2023-09-27 14:53:33 +0300

    Add score to Modal

--- main.js
+++ main.js
@@ -1,87 +1,95 @@
-const startPlay = document.querySelector('#startButton');
+const startPlay = document.querySelector("#startButton");
 console.log(startGame);
-const endPlay = document.querySelector('#endButton');
-const circles = document.querySelectorAll('.circle');// creates NodeList, simplified array. It is just collections of elements.
-const scoreDisplay = document.querySelector('.score');
+const endPlay = document.querySelector("#endButton");
+const circles = document.querySelectorAll(".circle"); // creates NodeList, simplified array. It is just collections of elements.
+const scoreDisplay = document.querySelector(".score");
+const overlay = document.querySelector(".overlayModal");
+const closeButton = document.querySelector(".close");
+const scoreModal = document.querySelector('#scoreModal');
 
 //global variables
-let score = 0;// to store our scores
+let score = 0; // to store our scores
 let timer;
-let pace = 1000;//miliseconds
+let pace = 1000; //miliseconds
 let active = 0;
 let rounds = 0;
 
 function getRundomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRundomNumber(0, 3));
+
+//To know what button was clicked. //Every time you click a circle you get a score
+const clickCircle = (i) => {
+  if (i !== active) {
+    /* if we have smth that is not highlited */ return endGame();
+  }
+  rounds--; // to be able to make more clicks than 3
+  score += 10;
+  scoreDisplay.textContent = score;
+};
+// classList.add(endgame)
+// classList.remove(endgame)
+
+circles.forEach((circle, i) => {
+  // it can be any name. i = index, in NodeList. Element has a placement which is index. Index can be changed all time. First parameter (circle) for itterate.
+  circle.addEventListener("click", () => clickCircle(i));
+}); // anonymos function inside. function can not be called before the function that was created
+
+const enableEvents = () => {
+  circles.forEach((circle) => {
+    circle.style.pointerEvents = "auto";
+  });
+}; // none by default
+
+// it waits for a second and them a new number will be showed. And do it again.
+function startGame() {
+  if (rounds >= 3) {
+    //if we start a game and do not click more than 3 times game ends
+    return endGame();
   }
 
-  console.log(getRundomNumber(0, 3));
+  enableEvents();
 
+  const newActive = pickNew(active); //gives a number and pass to function pickNew(active)
+  circles[newActive].classList.toggle("active");
+  circles[active].classList.remove("active");
 
-//To know what button was clicked.     //Every time you click a circle you get a score
-const clickCircle = (i) => {
-if (i !== active) // if we have smth that is not highlited
-{ return endGame() }
+  active = newActive; // overwrite with a new number
+  timer = setTimeout(startGame, pace);
 
-console.log('circle was clicked', i);
-rounds-- // to be able to make more clicks than 3
-score+=10;
-scoreDisplay.textContent = score;
-}
-   // classList.add(endgame)
-   // classList.remove(endgame)
-
-   
-circles.forEach((circle, i) => { // it can be any name. i = index, in NodeList. Element has a placement which is index. Index can be changed all time. First parameter (circle) for itterate.
-    circle.addEventListener('click', () => clickCircle(i))}) // anonymos function inside. function can not be called before the function that was created
-
-    const enableEvents = () => {
-        circles.forEach(circle => {
-            circle.computedStyleMap.pointEvents = "auto"
-        })
-    } // none by default
-
-      // it waits for a second and them a new number will be showed. And do it again.
-    function startGame() {
-if (rounds >= 3) //highlights 
-{
-    return endGame()
-}
-enableEvents();
-
-const newActive = pickNew(active) //gives a number and pass to function pickNew(active)
-circles[newActive].classList.toggle('active');
-circles[active].classList.remove('active');
-
-active = newActive; // overwrite with a new number
-
-timer = setTimeout(startGame, pace)
-
-pace -= 10
-rounds++
-       function pickNew(active) { //function will get a new number. will be return random number from 0 to 3
-        const newActive = getRundomNumber(0, 3);
-        console.log(newActive);
-        if (newActive !== active) {
-            return newActive  
-        }
-        return pickNew(active) // to loop until it is same
-       }
-       console.log(active);
+  pace -= 10;
+  rounds++;
+  function pickNew(active) {
+    //function will get a new number. will be return random number from 0 to 3
+    const newActive = getRundomNumber(0, 3);
+    console.log(newActive);
+    if (newActive !== active) {
+      return newActive;
     }
+    return pickNew(active); // to loop until it is same
+  }
+  console.log(active);
+}
 
 // end game
-    function endGame() {
-        console.log('end');
-        clearTimeout(timer)
-        resetGame();
-    }
+function endGame() {
+  console.log("end");
+  clearTimeout(timer);
+  modalShow();
+}
 
-    const resetGame = () => {
-        // buil-in method
-        window.location.reload()
-    }
-    startPlay.addEventListener('click', startGame);
-    endPlay.addEventListener('click', endGame);
+const resetGame = () => {
+  // buil-in method
+  window.location.reload();
+};
 
-  
+// Function to shaw the modal with the score
+const modalShow = () => {
+    overlay.classList.toggle("visible"); //Toggles between tokens in the list
+    scoreModal.textContent = score;
+};
+
+startPlay.addEventListener("click", startGame);
+endPlay.addEventListener("click", endGame);
+closeButton.addEventListener("click", resetGame);
